f4127e875445d0efb5e16e878749f93272f74b57
Lucas <lucas@dex360.net>
2025-06-28 23:15:17 -0300

    completar login

--- src/modules/auth/login/useScreenHooks.js
+++ src/modules/auth/login/useScreenHooks.js
@@ -1,20 +1,26 @@
-
 import { useState, useEffect } from "react";
 import { ApiService } from "./api";
 import { useGlobal } from "../../../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
+import { useSpinner } from "../../../context/SpinnerContext";
 
 export default function useScreenHooks() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { show, isLoading } = useSpinner();
   const { login } = useGlobal();
- 
-  async function handleSubmit( event) {
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    const response = await ApiService.login({ email, password });
-    login(response.token, response.user);
-    navigate("/pets");
+    try {
+      if (!isLoading) show();
+      const response = await ApiService.login({ email, password });
+      login(response.token, response.user);
+      navigate("/pets");
+    } catch (error) {
+      console.error("Error on login:", error);
+    }
   }
 
   useEffect(() => {
