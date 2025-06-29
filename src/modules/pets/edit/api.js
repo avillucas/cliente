cbe5c0dd3053a1e9e3a74443d7dc4eebae456b41
Lucas <lucas@dex360.net>
2025-06-29 12:40:38 -0300

    completando

--- src/modules/pets/edit/api.js
+++ src/modules/pets/edit/api.js
@@ -1,11 +1,24 @@
-
-import http from '../../../services/apiService';
-
-
+import http from "../../../services/apiService";
 const getPetById = async (id) => {
   const response = await http.get(`/pets/${id}`);
   return response.data;
 };
+const updatePet = async (petData) => {
+  if (
+    !petData ||
+    !petData.type ||
+    !petData.breed ||
+    !petData.name ||
+    !petData.age ||
+    !petData.size
+  ) {
+    throw new Error("Invalid pet data provided");
+  }
+  console.log("Updating pet with data:", petData);
+  const response = await http.post(`/pets/${petData.id}`, petData);
+  return response.data;
+};
 export const ApiService = {
   getPetById,
+  updatePet,
 };
