f37b2391fa21cd97ab59f134ed8376be83361187
Lucas <lucas@dex360.net>
2025-06-29 11:14:59 -0300

    correccion de pantalla

--- src/modules/pets/detail/DetailScreen.tsx
+++ src/modules/pets/detail/DetailScreen.tsx
@@ -1,5 +1,42 @@
-
 import React from "react";
+import useScreenHooks from "./useScreenHooks";
+import { Link } from "react-router-dom";
+
 export const DetailScreen: React.FC = () => {
-  return <div>Detail Screen</div>;
+  const { pet, errors, navigate } = useScreenHooks();
+  return (
+    <>
+      {pet && !errors && (
+        <div className="row">
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 className="h2">Sobre la mascota {pet.name}</h1>
+              <div className="btn-toolbar mb-2 mb-md-0">
+                <Link to="/pets" className="btn btn-sm btn-outline-secondary">
+                  Volver al listado
+                </Link>
+              </div>
+            </div>
+            <ul>
+              <li>
+                <strong>Nombre:</strong> {pet.name}
+              </li>
+              <li>
+                <strong>Edad:</strong> {pet.age}
+              </li>
+              <li>
+                <strong>Tipo:</strong> {pet.type}
+              </li>
+              <li>
+                <strong>Tamaño:</strong> {pet.size}
+              </li>
+              <li>
+                <strong>Raza:</strong> {pet.breed}
+              </li>
+            </ul>
+          </main>
+        </div>
+      )}
+    </>
+  );
 };
---
+++ src/modules/pets/detail/useScreenHooks.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { useSpinner } from "../../../context/SpinnerContext";
+import { useParams, useNavigate } from "react-router-dom";
+import { ApiService } from "./api";
+
+export default function useScreenHooks() {
+const [pet, setPet] = useState({
+  name: "",
+  age: 0,
+  type: "",
+  size: "",
+  breed: ""
+});
+  const [errors, setErrors] = useState(null);
+
+  const { show, hide, isLoading } = useSpinner();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = "Detalle Mascota";
+    const fetchPet = async () => {
+      if (!id) return;
+
+      try {
+        if (!isLoading) show();
+        const response = await ApiService.getPetById(Number(id));
+        setPet(response.payload);
+        hide();
+      } catch (error) {
+        setErrors(error);
+      }
+    };
+
+    fetchPet();
+  }, [id, hide, isLoading, show]);
+
+  return { pet, errors, navigate };
+}
